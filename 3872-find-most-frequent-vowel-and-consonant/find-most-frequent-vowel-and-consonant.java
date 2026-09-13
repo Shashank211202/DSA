e671a36c8a80b54d8ac1f3902b83446ca1fb92b4
Shashank Singh <141995938+Shashank211202@users.noreply.github.com>
2026-09-13 21:27:27 +0530

    Time: 4 ms (24.21%) | Memory: 44.1 MB (33.55%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.java
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.java
@@ -1,16 +1,17 @@
 class Solution {
-    public int maxFreqSum(String s) {        
-        int[] freqarr = new int[26];
+    public int maxFreqSum(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
         int maxvowel = 0;
         int maxconsonant = 0;
-        for(int t = 0; t < s.length(); t++){
-            freqarr[s.charAt(t) - 'a']++;
-        }
-        for(int j = 0; j < 26; j++){
-            if(j == 0 || j == 4 || j == 8 || j == 14 || j == 20 ){
-                  maxvowel = Math.max(maxvowel, freqarr[j]);
-            }else {
-                  maxconsonant = Math.max(maxconsonant, freqarr[j]);
+        for(char ch : map.keySet()){
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+                maxvowel = Math.max(maxvowel, map.get(ch));
+            }else{
+                maxconsonant = Math.max(maxconsonant, map.get(ch));
             }
         }
         return maxvowel + maxconsonant;
