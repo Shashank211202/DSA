3a4289a29cf2c0eb43e1ae3f473f55950eb01c1d
Shashank Singh <141995938+Shashank211202@users.noreply.github.com>
2026-09-13 19:47:49 +0530

    Time: 1 ms (100.00%) | Memory: 43.8 MB (65.63%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.java
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.java
@@ -3,14 +3,14 @@ class Solution {
         int[] freqarr = new int[26];
         int maxvowel = 0;
         int maxconsonant = 0;
-        for(int i = 0; i < s.length(); i++){
-            freqarr[s.charAt(i) - 'a']++;
+        for(int t = 0; t < s.length(); t++){
+            freqarr[s.charAt(t) - 'a']++;
         }
-        for(int i = 0; i < 26; i++){
-            if(i == 0 || i == 4 || i == 8 || i == 14 || i == 20 ){
-                  maxvowel = Math.max(maxvowel, freqarr[i]);
+        for(int j = 0; j < 26; j++){
+            if(j == 0 || j == 4 || j == 8 || j == 14 || j == 20 ){
+                  maxvowel = Math.max(maxvowel, freqarr[j]);
             }else {
-                  maxconsonant = Math.max(maxconsonant, freqarr[i]);
+                  maxconsonant = Math.max(maxconsonant, freqarr[j]);
             }
         }
         return maxvowel + maxconsonant;
