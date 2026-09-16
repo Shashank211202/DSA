0683f2d702742a6293f55a4c3dfdb4b949046313
Shashank Singh <141995938+Shashank211202@users.noreply.github.com>
2026-09-16 15:02:46 +0530

    Time: 1 ms (96.05%) | Memory: 44.3 MB (97.37%) - LeetSync

--- 541-reverse-string-ii/reverse-string-ii.java
+++ 541-reverse-string-ii/reverse-string-ii.java
@@ -1,28 +1,24 @@
-class Solution {
-
-    public String reverseStr(String s, int k) {
-
-        char[] arr = s.toCharArray();
-
-        for (int i = 0; i < s.length(); i += 2 * k) {
-
-            // Number of characters to reverse
-            int n = Math.min(k, s.length() - i);
-
-            // Number of swaps required
-            int mid = n / 2;
-
-            for (int j = 0; j < mid; j++) {
-
-                int left = i + j;
-                int right = i + n - 1 - j;
-
-                char temp = arr[left];
-                arr[left] = arr[right];
-                arr[right] = temp;
-            }
+class Solution { 
+    public void reverse(char[] s, int left, int right) { 
+        while(left < right) { 
+            char temp = s[left]; 
+            s[left] = s[right]; 
+            s[right] = temp; 
+            
+            left++;
+            right--;
+        } 
+    } 
+    public String reverseStr(String s, int k) { 
+        char[] arr = s.toCharArray(); 
+        if(s.length() < k)
+            reverse(arr, 0, s.length() - 1); 
+        else {
+            for(int i = 0; i < s.length(); i += 2 * k) { 
+                int end = Math.min(i + k - 1, s.length() - 1);
+                reverse(arr, i, end); 
+            } 
         }
-
-        return new String(arr);
-    }
+        return new String(arr); 
+    } 
 }
