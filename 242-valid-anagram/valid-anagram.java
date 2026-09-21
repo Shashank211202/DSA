2393a7a867f9174bc773899212841e1812badbfb
Shashank Singh <141995938+Shashank211202@users.noreply.github.com>
2026-09-21 22:45:02 +0530

    Time: 15 ms (23.89%) | Memory: 46.6 MB (24.56%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,15 +1,23 @@
-import java.util.Arrays;
-
-class Solution {
-
-    public boolean isAnagram(String s, String t) {
-
-        char[] arr1 = s.toCharArray();
-        char[] arr2 = t.toCharArray();
-
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
-
-        return Arrays.equals(arr1, arr2);
-    }
-}
+import java.util.HashMap;
+  
+  public class Solution {
+      public boolean isAnagram(String s, String t) {
+          if (s.length() != t.length()) return false;
+  
+          HashMap<Character, Integer> map = new HashMap<>();
+  
+          for (char c : s.toCharArray()) {
+              map.put(c, map.getOrDefault(c, 0) + 1);
+          }
+  
+          for (char c : t.toCharArray()) {
+              if (!map.containsKey(c) || map.get(c) == 0) {
+                  return false;
+              }
+              map.put(c, map.get(c) - 1);
+          }
+  
+          return true;
+      }
+  }
+      
